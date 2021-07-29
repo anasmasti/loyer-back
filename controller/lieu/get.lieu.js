eede6f5c7ff545e6296ff8651ed269126e3c8c01
badrelazzaby <badr.azzaby411@gmail.com>
2021-07-29 17:27:55 +0100

    init commit

--- controller/lieu/get.lieu.js
+++ controller/lieu/get.lieu.js
@@ -4,68 +4,160 @@ const mongoose = require('mongoose')
 
 
 module.exports = {
-    
+
     //get all lieus
     getAllLieu: async (req, res) => {
-        await Lieu.find({ deleted: false })
-            .then((data) => {
+        await Lieu.aggregate([
+            {
+                $match: {
+                    "deleted": false,
+                }
+            },
+            {
+                $addFields: {
+                    "amenagement": {
+                        $map: {
+                            input: {
+                                $filter: {
+                                    input: "$amenagement",
+                                    as: "amenagementfillter",
+                                    cond: { $eq: ["$$amenagementfillter.deleted", false] }
+                                }
+                            },
+                            as: "amenagementmap",
+                            in: {
+                                "nature_amenagement": "$$amenagementmap.nature_amenagement",
+                                "deleted": "$$amenagementmap.deleted",
+                                "_id": "$$amenagementmap._id",
+                                "montant_amenagement": "$$amenagementmap.montant_amenagement",
+                                "valeur_nature_chargeProprietaire": "$$amenagementmap.valeur_nature_chargeProprietaire",
+                                "valeur_nature_chargeFondation": "$$amenagementmap.valeur_nature_chargeFondation",
+                                "numero_facture": "$$amenagementmap.numero_facture",
+                                "numero_bon_commande": "$$amenagementmap.numero_bon_commande",
+                                "date_passation_commande" : "$$amenagementmap.date_passation_commande",
+                                "evaluation_fournisseur" : "$$amenagementmap.evaluation_fournisseur",
+                                "date_fin_travaux" : "$$amenagementmap.date_fin_travaux",
+                                "images_apres_travaux" : "$$amenagementmap.images_apres_travaux",
+                                "croquis_travaux" : "$$amenagementmap.croquis_travaux",
+                                "fournisseur": {
+                                    $filter: {
+                                        input: "$$amenagementmap.fournisseur",
+                                        as: "fournisseur",
+                                        cond: { $eq: ["$$fournisseur.deleted", false] }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "directeur_regional": {
+                        $map: {
+                            input: {
+                                $filter: {
+                                    input: "$directeur_regional",
+                                    as: "directeur_regionalFillter",
+                                    cond: { $eq: ["$$directeur_regionalFillter.deleted_directeur", false] }
+                                }
+                            },
+                            as: "directeur_regionalMap",
+                            in: {
+
+                                "deleted_directeur": "$$directeur_regionalMap.deleted_directeur",
+                                "_id": "$$directeur_regionalMap._id",
+                                "matricule": "$$directeur_regionalMap.matricule",
+                                "nom": "$$directeur_regionalMap.nom",
+                                "prenom": "$$directeur_regionalMap.prenom"
+                            }
+                        }
+                    }
+                }
+            }
+
+        ])
+            .then(data => {
                 res.json(data)
             })
-            .catch((error) => {
-                res.status(400).send({ message: error.message })
+            .catch(error => {
+                res.status(402).send({ message: error.message })
             })
     },
 
     // get Lieu by Id
     getLieuById: async (req, res) => {
-        await Lieu.findById({ _id: req.params.Id })
-            .then((data) => {
+        await Lieu.aggregate([
+            {
+                $match: {
+                    _id: mongoose.Types.ObjectId(req.params.Id),
+                    "deleted": false  
+                }
+            },
+            {
+                $addFields: {
+                    "amenagement": {
+                        $map: {
+                            input: {
+                                $filter: {
+                                    input: "$amenagement",
+                                    as: "amenagementfillter",
+                                    cond: { $eq: ["$$amenagementfillter.deleted", false] }
+                                }
+                            },
+                            as: "amenagementmap",
+                            in: {
+                                "nature_amenagement": "$$amenagementmap.nature_amenagement",
+                                "deleted": "$$amenagementmap.deleted",
+                                "_id": "$$amenagementmap._id",
+                                "montant_amenagement": "$$amenagementmap.montant_amenagement",
+                                "valeur_nature_chargeProprietaire": "$$amenagementmap.valeur_nature_chargeProprietaire",
+                                "valeur_nature_chargeFondation": "$$amenagementmap.valeur_nature_chargeFondation",
+                                "numero_facture": "$$amenagementmap.numero_facture",
+                                "numero_bon_commande": "$$amenagementmap.numero_bon_commande",
+                                "date_passation_commande" : "$$amenagementmap.date_passation_commande",
+                                "evaluation_fournisseur" : "$$amenagementmap.evaluation_fournisseur",
+                                "date_fin_travaux" : "$$amenagementmap.date_fin_travaux",
+                                "images_apres_travaux" : "$$amenagementmap.images_apres_travaux",
+                                "croquis_travaux" : "$$amenagementmap.croquis_travaux",
+                                "fournisseur": {
+                                    $filter: {
+                                        input: "$$amenagementmap.fournisseur",
+                                        as: "fournisseur",
+                                        cond: { $eq: ["$$fournisseur.deleted", false] }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "directeur_regional": {
+                        $map: {
+                            input: {
+                                $filter: {
+                                    input: "$directeur_regional",
+                                    as: "directeur_regionalFillter",
+                                    cond: { $eq: ["$$directeur_regionalFillter.deleted_directeur", false] }
+                                }
+                            },
+                            as: "directeur_regionalMap",
+                            in: {
+
+                                "deleted_directeur": "$$directeur_regionalMap.deleted_directeur",
+                                "_id": "$$directeur_regionalMap._id",
+                                "matricule": "$$directeur_regionalMap.matricule",
+                                "nom": "$$directeur_regionalMap.nom",
+                                "prenom": "$$directeur_regionalMap.prenom"
+                            }
+                        }
+                    }
+                }
+            }
+
+        ])
+            .then(data => {
                 res.json(data)
             })
-            .catch((error) => {
+            .catch(error => {
                 res.status(402).send({ message: error.message })
             })
     },
-
-    //get amenagements by Id
-    getAmenagementById: async (req, res) => {
-        await Lieu.find({ _id: req.params.IdLieu }, { amenagements: { $elemMatch: { _id: req.params.IdAmng } } })
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((error) => {
-                res.status(404).send({ message: error.message })
-            })
-    },
-
-    //get all amenagements that has deleted false
-    getAllAmenagement: async (req, res) => {
-        await Lieu.find({}, { amenagement: { $elemMatch: { deleted: false } } }, { amenagement: 1 })
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((error) => {
-                res.status(404).send({ message: error.message })
-            })
-    },
-
-    //get not deleted fournisseurs of a Lieu ID 
-    getFournisseursOfLieu: async (req, res) => {
-        await Lieu.aggregate([
-            { $match: { '_id': mongoose.Types.ObjectId(req.params.IdLieu) } },
-            { $unwind: '$amenagement' },
-            { $match: { 'amenagement.deleted': false } },
-            { $unwind: '$amenagement.fournisseur' },
-            { $match: { 'amenagement.fournisseur.deleted': false } },
-            { $project: { 'amenagement.fournisseur': 1 } }
-        ])
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((error) => {
-                res.status(404).send({ message: error.message })
-            })
-    },
+   
 
     //get all Directions regionals and Supervision
     getAllDirectionsAndSupervions: async (req, res) => {
@@ -83,7 +175,7 @@ module.exports = {
             res.status(404).send({ message: error.message })
         }
     },
-    
+
     // get coute lieu
     getCountLieu: async (req, res) => {
         await Lieu.countDocuments({ deleted: false })
@@ -93,5 +185,7 @@ module.exports = {
             .catch(error => {
                 res.status(402).send({ message: error.message })
             })
-    }
+    },  
 }
+
+
