2fbc4dd332a565e7d062952d2bb56cf88f548164
badrelazzaby <badr.azzaby411@gmail.com>
2021-09-20 10:43:20 +0100

    init

--- controller/user-roles/put.roles.js
+++ controller/user-roles/put.roles.js
@@ -1,28 +1,30 @@
-const userRoles = require('../../models/roles/roles.model')
+const userRoles = require("../../models/roles/roles.model");
 
 module.exports = {
-    updateUserRoles: async (req, res) => {
-        let item = 0;
-        let allUserRoles = []
-        for (item in req.body.userRoles) {
-            await allUserRoles.push({
-                roleName: req.body.userRoles[item].roleName,
-                deleted:req.body.deleted
-            })
-        }
-        await userRoles.findByIdAndUpdate(req.params.Id,
-            {
-                userMatricul: req.body.userMatricul,
-                nom: req.body.nom,
-                prenom: req.body.prenom,
-                userRoles: allUserRoles,
-                deleted:req.body.deleted
-            })
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((error => {
-                res.status(400).send({ message: error.message } || "Can't Update User Roles")
-            }))
+  updateUserRoles: async (req, res) => {
+    let item = 0;
+    let allUserRoles = [];
+    for (item in req.body.userRoles) {
+      allUserRoles.push({
+        roleName: req.body.userRoles[item].roleName,
+        deleted: req.body.userRoles[item].deleted,
+      });
     }
-}
+    await userRoles
+      .findByIdAndUpdate(req.params.Id, {
+        userMatricul: req.body.userMatricul,
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        userRoles: allUserRoles,
+        deleted: req.body.deleted,
+      })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res
+          .status(400)
+          .send({ message: error.message } || "Can't Update User Roles");
+      });
+  },
+};
