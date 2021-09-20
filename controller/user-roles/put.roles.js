a5b222e3c3937a6029dbae966eab92f639dedb5d
badrelazzaby <badr.azzaby411@gmail.com>
2021-09-20 10:45:22 +0100

    set the index of deleted roles in update userRoles

--- controller/user-roles/put.roles.js
+++ controller/user-roles/put.roles.js
@@ -2,12 +2,13 @@ const userRoles = require('../../models/roles/roles.model')
 
 module.exports = {
     updateUserRoles: async (req, res) => {
+        console.log(req.body);
         let item = 0;
         let allUserRoles = []
         for (item in req.body.userRoles) {
             await allUserRoles.push({
                 roleName: req.body.userRoles[item].roleName,
-                deleted:req.body.deleted
+                deleted:req.body.userRoles[item].deleted
             })
         }
         await userRoles.findByIdAndUpdate(req.params.Id,
