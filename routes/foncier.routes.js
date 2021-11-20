71a23710f37f0e4d2712a21f0991a4e4e178ae61
badrelazzaby <badr.azzaby411@gmail.com>
2021-11-20 11:18:21 +0100

    ajouter verification des roles dans les routes de foncier

--- routes/foncier.routes.js
+++ routes/foncier.routes.js
@@ -3,25 +3,36 @@ const postFoncier = require("../controllers/foncier/post.foncier");
 const putFoncier = require("../controllers/foncier/put.foncier");
 const getFoncier = require("../controllers/foncier/get.foncier");
 const deleteFoncier = require("../controllers/foncier/delete.foncier");
+const verifyRole = require("../middleware/verify-user-role");
 const upload = require("../middleware/upload");
 
 const router = express.Router();
-router.route("/foncier/ajouter").post(
+router.route("/foncier/ajouter/:matricule").post(
+  verifyRole.checkRoles("CDGSP", "CSLA"),
   upload.fields([
     { name: "imgs_lieu_entrer", maxCount: 5 },
     { name: "imgs_amenagement", maxCount: 5 },
     { name: "imgs_croquis", maxCount: 5 },
-  ]), postFoncier.ajouterFoncier
+  ]),
+  postFoncier.ajouterFoncier
 );
-router.route("/foncier/modifier/:IdFoncier").put(
+router.route("/foncier/modifier/:IdFoncier/:matricule").put(
+  verifyRole.checkRoles("CDGSP", "CSLA"),
   upload.fields([
     { name: "imgs_lieu_entrer", maxCount: 5 },
     { name: "imgs_amenagement", maxCount: 5 },
     { name: "imgs_croquis", maxCount: 5 },
-  ]), putFoncier.modifierFoncier
+  ]),
+  putFoncier.modifierFoncier
 );
-router.route("/foncier/all").get(getFoncier.getAllFoncier);
-router.route("/foncier/detail/:IdFoncier").get(getFoncier.getFoncierById);
-router.route("/foncier/supprimer/:IdFoncier").put(deleteFoncier.deleteFoncier);
+router
+  .route("/foncier/all/:matricule")
+  .get(verifyRole.checkRoles("CDGSP", "CSLA"), getFoncier.getAllFoncier);
+router
+  .route("/foncier/detail/:IdFoncier/:matricule")
+  .get(verifyRole.checkRoles("CDGSP", "CSLA"), getFoncier.getFoncierById);
+router
+  .route("/foncier/supprimer/:IdFoncier/:matricule")
+  .put(verifyRole.checkRoles("CDGSP", "CSLA"), deleteFoncier.deleteFoncier);
 
-module.exports = router
+module.exports = router;
