6005b01d759d904a7f12dc5b0724070057984b05
YassineAm00 <yassine.19am49@gmail.com>
2022-04-26 09:44:10 +0000

    Merge chenges

---
+++ controllers/affectation_proprietaire/get.affectationproprietaire.js
@@ -0,0 +1,50 @@
+const Proprietaire = require("../../models/proprietaire/proprietaire.model");
+const Contrat = require("../../models/contrat/contrat.model");
+const AffectationProprietaire = require("../../models/affectation_proprietaire/affectation_proprietaire.schema");
+
+module.exports = {
+  getAllAffectationsProprietaire: async (req, res) => {
+    await AffectationProprietaire.find({ deleted: false })
+      .populate({
+        path: "proprietaire",
+      })
+      .populate({
+        path: "contrat",
+      })
+      .sort({ updatedAt: "desc" })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.status(402).send({ message: error.message });
+      });
+  },
+
+  getAffectationProprietairePerID: async (req, res) => {
+    await AffectationProprietaire.findById(req.params.Id)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((error) => {
+        res.status(500).send({ message: `Aucun Propriétaire trouvé` || error });
+      });
+  },
+
+  getProprietairesHasntMondataire: async (req, res) => {
+    Contrat.findById({ _id: req.params.IdContrat })
+      .populate({ path: "proprietaires", populate: { path: "proprietaire" } })
+      .then((contrat) => {
+        let proprietairesHasntMondataire = contrat.proprietaires.filter(
+          (proprietaire) => {
+            return (
+              !proprietaire.is_mandataire && proprietaire.has_mandataire == null
+            );
+          }
+        );
+        res.json(proprietairesHasntMondataire);
+      })
+      .catch((error) => {
+        res.status(500).send({ message: `Aucun contrat trouvé` || error });
+      });
+  },
+};
